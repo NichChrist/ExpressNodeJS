dbbe98aa11edb870f45b1da9526e26a5a832c7ed
nicholaschristian <nicholas.christian@concise.co.id>
2024-03-28 15:23:32 +0700

    fix: 🐛 Fixed Uom Model

--- src/models/Uom.js
+++ src/models/Uom.js
@@ -7,6 +7,20 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
+        static associate(models) {
+            // define association here
+            Uom.hasMany(models.uom_conversions, {
+                foreignKey: 'uoms_from_id',
+                sourceKey: 'id',
+                as: 'uoms_conversion_from',
+            });
+            
+            Uom.hasMany(models.uom_conversions, {
+                foreignKey: 'uoms_to_id',
+                sourceKey: 'id',
+                as: 'uoms_conversion_to',
+            });
+        }
     }
 
     Uom.init(
