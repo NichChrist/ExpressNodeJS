ae9cc16849b7e6d9083d5b17beb22ff6f91449cf
nicholaschristian <nicholas.christian@concise.co.id>
2024-04-17 16:27:37 +0700

    feat: 🎸 added ingredients migration/model

---
+++ src/db/migrations/20240417082108-create-ingredients.js
@@ -0,0 +1,32 @@
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('ingredients', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				unique: true,
+			},
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+			created_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updated_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			deleted_at: {
+				allowNull: true,
+				type: Sequelize.DATE,
+			}
+		});
+	},
+
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.dropTable('ingredients');
+  },
+};
---
+++ src/models/Ingredient.js
@@ -0,0 +1,40 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Ingredient extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+
+        }
+    }
+
+    Ingredient.init(
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }           
+        },
+        {
+            sequelize,
+            modelName: 'ingredient',
+            underscored: true,
+            paranoid: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+            deletedAt: 'deleted_at',
+        }
+    );
+    return Ingredient;
+};
