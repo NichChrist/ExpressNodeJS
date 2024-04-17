ce60cb5388cc6f0f03ed2dcdaf43cd8f3baaacd6
nicholaschristian <nicholas.christian@concise.co.id>
2024-04-17 14:44:15 +0700

    feat: 🎸 added payment_methods migration/model

---
+++ src/models/PaymentMethod.js
@@ -0,0 +1,43 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class PaymentMethod extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+
+    PaymentMethod.init(
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
+        },
+        {
+            sequelize,
+            modelName: 'payment_method',
+            underscored: true,
+            paranoid: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+            deletedAt: 'deleted_at',
+        }
+    );
+    return PaymentMethod;
+};
