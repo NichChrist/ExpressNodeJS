7bb791796ebef4ebd0f344d061971e37be0e6bd1
nicholaschristian <nicholas.christian@concise.co.id>
2024-04-18 11:27:51 +0700

    fix: 🐛 fixed nullables in migrations

--- src/db/migrations/20240326072024-add-outlet-id-in-user-table.js
+++ src/db/migrations/20240326072024-add-outlet-id-in-user-table.js
@@ -2,6 +2,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'outlet_id', {
       type: Sequelize.UUID,
+      allowNull: false,
       references: {
         model: 'outlets',
         key: 'id'
--- src/db/migrations/20240328052312-create-uoms.js
+++ src/db/migrations/20240328052312-create-uoms.js
@@ -12,7 +12,7 @@ module.exports = {
 				type: Sequelize.STRING,
 			},
 			metric_code: {
-				allowNull: false,
+				allowNull: true,
 				type: Sequelize.STRING,
 			},
 			created_at: {
